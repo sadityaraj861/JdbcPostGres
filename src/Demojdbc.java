9ebcf1a9bb64d02564b0e737fca2cae883770f65
sadityaraj861 <sadityaraj861@gmail.com>
2025-04-14 17:10:35 +0530

    Chup

---
+++ src/Demojdbc.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class Demojdbc {
+    public static void main(String[] args) throws Exception {
+        /*
+        * import package
+        * load and register
+        * create connection
+        * create statement
+        * execute state,ent
+        * process and results
+        * close
+         */
+
+        String url="jdbc:postgresql://localhost:5432/demo"; //default portno of postgres  is 5432.
+        String Uname="postgres";
+        String pass="Asdf@1234";
+
+        Class.forName("org.postgresql.Driver");
+        Connection con=DriverManager.getConnection(url,Uname,pass);
+
+        System.out.println("Connection Established");
+
+    }
+}
